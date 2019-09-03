60ae723d0833c278db6428e078d2c4fc2961ce47
chen <wszgrcy@gmail.com>
2019-09-03 15:55:39 +0800

    声明类型

---
+++ lib/src/define/dynamic.class.ts
@@ -0,0 +1,5 @@
+import { Type } from '@angular/core';
+
+export interface DynamicEntryPoint {
+    entry: Type<any>
+}
--- lib/src/define/index.ts
+++ lib/src/define/index.ts
@@ -1,3 +1,4 @@
 export { AccurateTime } from './date-picker.define'
 export { LOADING_PROGRESS, SPINNER_CONFIG } from './token'
 export * from './fablist-directive.define'
+export * from './dynamic.class'
