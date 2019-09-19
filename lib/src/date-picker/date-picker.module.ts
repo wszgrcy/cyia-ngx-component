47bb1fe7a6375b3a3c0e4f90e78d02c320d52cdb
chen <wszgrcy@gmail.com>
2019-09-19 17:53:54 +0800

    日期格式传入

--- lib/src/date-picker/date-picker.module.ts
+++ lib/src/date-picker/date-picker.module.ts
@@ -3,7 +3,7 @@ import { MatFormFieldModule } from '@angular/material/form-field';
 import { NgModule } from '@angular/core';
 import { CyiaDatePickerComponent } from './date-picker.component';
 import { ModuleWithProviders } from '@angular/compiler/src/core';
-import { MAT_DATE_LOCALE, DateAdapter, MAT_DATE_FORMATS } from "@angular/material/core";
+import { MAT_DATE_LOCALE, DateAdapter, MAT_DATE_FORMATS, MatDateFormats } from "@angular/material/core";
 import { MatInputModule } from "@angular/material/input";
 import { MomentDateAdapter, MAT_MOMENT_DATE_FORMATS } from '@angular/material-moment-adapter';
 import { MatDatepickerModule } from '@angular/material/datepicker';
@@ -30,13 +30,13 @@ import { FormsModule } from '@angular/forms';
   providers: []
 })
 export class CyiaDatePickerModule {
-  static forRoot(language: string): ModuleWithProviders {
+  static forRoot(language: string, matDateFormats: MatDateFormats = MAT_MOMENT_DATE_FORMATS): ModuleWithProviders {
     return {
       ngModule: CyiaDatePickerModule,
       providers: [
         { provide: MAT_DATE_LOCALE, useValue: language },
         { provide: DateAdapter, useClass: MomentDateAdapter, deps: [MAT_DATE_LOCALE] },
-        { provide: MAT_DATE_FORMATS, useValue: MAT_MOMENT_DATE_FORMATS },
+        { provide: MAT_DATE_FORMATS, useValue: matDateFormats },
       ]
     }
   }
