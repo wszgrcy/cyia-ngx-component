03da6fd01dd079f43eff3e180f741898bfb1b5b3
chen <wszgrcy@gmail.com>
2019-10-30 14:17:24 +0800

    自定义选择器:懒加载+附加

--- lib/src/custompicker/custompicker-wrapper/custompicker-wrapper.component.ts
+++ lib/src/custompicker/custompicker-wrapper/custompicker-wrapper.component.ts
@@ -1,8 +1,8 @@
-import { Component, OnInit, ChangeDetectionStrategy, ContentChild, TemplateRef, Input, Type } from '@angular/core';
+import { Component, OnInit, ChangeDetectionStrategy, ContentChild, TemplateRef, Input, Type, Optional, Compiler, NgModuleFactory, Injector } from '@angular/core';
 import { MatDialog } from '@angular/material/dialog';
 import { Subject, merge, BehaviorSubject } from 'rxjs';
 import { take, filter } from 'rxjs/operators';
-import { Route } from '@angular/router';
+import { Route, LoadChildrenCallback } from '@angular/router';
 import { OverlayConfig, OverlayRef, Overlay, PositionStrategy, ScrollStrategy } from '@angular/cdk/overlay';
 import { CustompickerInput } from '../custompicker-input.directive';
 import { ComponentPortal } from '@angular/cdk/portal';
@@ -15,14 +15,13 @@ import { Custompicker } from '../class/custompicker.class';
   changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CustompickerWrapperComponent implements OnInit {
+
   /**
-   * 这个应该接收组件用的
-   */
-  /**
-   * todo 懒加载模块
+   * ! 懒加载+弹窗时,由于没有传递相关参数,会报错,所以只能懒加载+附加模式运行
+   * todo 有时间给dialog提pr
    * 
    */
-  @Input() path: Route['loadChildren']
+  @Input() path: LoadChildrenCallback
   @Input() component: Type<Custompicker>
   /**0弹窗还是1附加 */
   @Input() mode = 1
@@ -33,9 +32,12 @@ export class CustompickerWrapperComponent implements OnInit {
   /**ngModel传入时保存数据 */
   inputChange = new BehaviorSubject(undefined)
   overlayRef: OverlayRef
+  componentFactoryResolver
   constructor(
     private matDialog: MatDialog,
-    private overlay: Overlay
+    private overlay: Overlay,
+    @Optional() private compiler: Compiler,
+    private injector: Injector
   ) { }
 
   ngOnInit() { }
@@ -46,14 +48,22 @@ export class CustompickerWrapperComponent implements OnInit {
    * @memberof CustompickerWrapperComponent
    */
   async open() {
+    this.component = await this.lazyLoadComponent()
     if (this.mode == 0) {
       this.openDialog()
     } else {
       this.openPopup()
     }
   }
+  async lazyLoadComponent(): Promise<Type<any>> {
+    if (!this.path) return undefined
+    let module = await this.path()
+    const ngModuleFactory = module instanceof NgModuleFactory ? module : await this.compiler.compileModuleAsync(module)
+    const ngModuleRef = ngModuleFactory.create(this.injector)
+    this.componentFactoryResolver = ngModuleRef.componentFactoryResolver
+    return ngModuleRef.instance.entry
+  }
   async openDialog() {
-
     let ref = this.matDialog.open(
       this.component, {
         //doc 将[ngModel]传入
@@ -77,7 +87,7 @@ export class CustompickerWrapperComponent implements OnInit {
       panelClass: 'mat-datepicker-popup',
       // scrollStrategy:()=>ScrollStrategy
     })
-    let ref = this.overlayRef.attach(new ComponentPortal(this.component))
+    let ref = this.overlayRef.attach(new ComponentPortal(this.component, undefined, undefined, this.componentFactoryResolver))
     ref.instance.overlayRef = this.overlayRef
     //doc 背景点击关闭
     this.overlayRef.backdropClick().subscribe(() => this.overlayRef.detach())
